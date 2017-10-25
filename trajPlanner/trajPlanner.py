b452f6e4f65ba3179e5fda0c4cc7b8b62347f7fd
Mahesh Kumar Karikalan <maheshku@uni-bremen.de>
2017-10-25 18:58:43 +0200

    working state of osqp and qpoases for multi dof

--- trajPlanner/trajPlanner.py
+++ trajPlanner/trajPlanner.py
@@ -92,16 +92,19 @@ class TrajectoryPlanner:
                 self.ub.append(np.hstack([self.sqp[i].ubG, self.sqp[i].b, self.sqp[i].b, self.sqp[i].ub]))
 
             else:
-                # self.C.append(np.vstack([self.sqp[i].G, self.sqp[i].A]))
-                self.C.append(self.sqp[i].G)
+                self.C.append(np.vstack([self.sqp[i].G, self.sqp[i].A]))
+                # self.C.append(self.sqp[i].G)
                 self.A.append(self.sqp[i].A)
 
                 # print np.array([self.sqp[i].lbG[0][0]])
-                self.lb.append(np.hstack([self.sqp[i].lbG]))
-                self.lb = np.dstack([self.lb, self.sqp[i].lbG[0][0]])
+                # self.lb.append(np.hstack([self.sqp[i].lbG]))
+                self.lb.append(self.sqp[i].lb)
+
+                # self.lb = np.append(self.lb, self.sqp[i].lbG[0][0])
                 # self.ub.append(self.sqp[i].ub.tolist())
                 self.ub.append(np.hstack([self.sqp[i].ub]))
-                self.ub = np.dstack([self.ub, self.sqp[i].ub[0][0]])
+                # self.ub = np.dstack([self.ub, self.sqp[i].ub[0][0]])
+
 
 
             self.lbC.append(self.sqp[i].lbG.tolist())
@@ -110,6 +113,8 @@ class TrajectoryPlanner:
             # sp.display()
             # print (self.lb)
 
+        # print self.C
+        # print self.A
 
         # print self.G
         # self.q = self.q[0]
@@ -213,75 +218,6 @@ class TrajectoryPlanner:
         print ("maxNoOfIteration")
         print (self.maxNoOfIteration)
 
-    def solveQp(self):
-        # options = Options()
-        # options.printLevel = PrintLevel.MEDIUM
-
-        # qp = QProblem(self.H.shape[0], self.A.shape[0])
-        # qp.setOptions(options)
-
-        # H = np.array([1.0, 0.0, 0.0, 0.5]).reshape((2, 2))
-        # A = np.array([1.0, 1.0]).reshape((2, 1))
-        # g = np.array([1.5, 1.0])
-        # lb = np.array([0.5, -2.0])
-        # ub = np.array([5.0, 2.0])
-        # lbA = np.array([-1.0])
-        # ubA = np.array([2.0])
-        #
-        # # Setup data of second QP.
-        #
-        # g_new = np.array([1.0, 1.5])
-        # lb_new = np.array([0.0, -1.0])
-        # ub_new = np.array([5.0, -0.5])
-        # lbA_new = np.array([-2.0])
-        # ubA_new = np.array([1.0])
-        #
-        # # Setting up QProblem object.
-        # C = vstack([self.G, self.A, self.A])
-
-        # print "self.H.shape[0], self.A.shape[0]", self.H.shape[0], self.A.shape[0]
-        qp = QProblem(self.P.shape[0], self.q.shape[0])
-        options = Options()
-        options.setToMPC()
-        options.printLevel = PrintLevel.LOW
-        qp.setOptions(options)
-
-        # print "before init"
-        # self.display()
-
-        # temp =  np.asarray(self.initialGuess)
-
-        # print temp
-
-        # TODO: check return value for error code; throw exception if unsuccessful
-
-        if self.isInitialGuessAvailable:
-            status =  qp.init(self.P, self.G, self.q, self.lb, self.ub, self.lbC, self.ubC, np.array([self.maxNoOfIteration]), 0.0, np.array([self.initialGuess]).flatten())
-        else:
-            status = qp.init1(self.P, self.G, self.q, self.lb, self.ub, self.lbC, self.ubC,
-                              np.array([self.maxNoOfIteration]))
-        # if (status == 0):
-        print ("init status: ", status)
-
-        # print "before hotstart"
-        # self.display()
-
-        self.q = self.q.flatten()
-        # TODO: check return value for error code; throw exception if unsuccessful
-        status = qp.hotstart(self.G, self.lb, self.ub, self.lbC, self.ubC, np.array([self.maxNoOfIteration]))
-
-        print ("hotstart status: ", status)
-
-        # assert type(status) is type(int), "Cannot solver for given number of duration"
-
-        # if not (status):
-        #     print "Cannot solver for given number of duration"
-
-        # status  = qp.getSilmpleStatus();
-
-
-        return qp, self.P.shape[0]
-
 
     def solveProblem(self):
 
@@ -301,19 +237,20 @@ class TrajectoryPlanner:
             return  result, sol
 
 
+        elif self.solver == "osqp1":
+            from qpsolvers.qpsolvers import qpoases_ as qp
+            qp = qp.qpoases_solve_qp(self.P, self.q, self.C, self.lb, self.ub, self.lbC, self.ubC, self.A, self.b, initvals=None,
+                                      max_wsr=np.array([self.maxNoOfIteration]))
+
+            x_opt = np.zeros(self.P.shape[0])
+            ret = qp.getPrimalSolution(x_opt)
+
+            if ret != 0:  # 0 == SUCCESSFUL_RETURN code of qpOASES
+                warn("qpOASES failed with return code %d" % ret)
+
+            print "numJoints", self.numJoints
+            print np.split(x_opt, self.numJoints)
         else:
-            # from qpsolvers.qpsolvers import qpoases_ as qp
-            # qp = qp.qpoases_solve_qp(self.P, self.q, self.C, self.lb, self.ub, self.lbG, self.ubG, self.A, self.b, initvals=None,
-            #                           max_wsr=np.array([self.maxNoOfIteration]))
-            #
-            # x_opt = np.zeros(self.P.shape[0])
-            # ret = qp.getPrimalSolution(x_opt)
-            #
-            # if ret != 0:  # 0 == SUCCESSFUL_RETURN code of qpOASES
-            #     warn("qpOASES failed with return code %d" % ret)
-            #
-            # print "numJoints", self.numJoints
-            # print np.split(x_opt, self.numJoints)
 
             from qpsolvers.qpsolvers import cvxopt_ as qp
 

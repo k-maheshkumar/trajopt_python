2ba8eadb160050e2c20a3ed04c7e1e009f64ef2e
Mahesh Kumar Karikalan <maheshku@uni-bremen.de>
2017-12-15 19:02:36 +0100

    updated gui with dynamic params

--- scripts/GUI/TrajPlanner.py
+++ scripts/GUI/TrajPlanner.py
@@ -3,11 +3,10 @@ from PyQt4 import QtGui, QtCore
 import yaml
 import numpy as np
 from scripts.utils import yaml_paser as yaml
-
-
+import itertools
 class PlannerGui(QtGui.QWidget):
     def __init__(self):
-        super(Example, self).__init__()
+        super(PlannerGui, self).__init__()
 
         file_path_prefix = '../../config/'
         sqp_config_file = file_path_prefix + 'sqp_config.yaml'
@@ -17,69 +16,101 @@ class PlannerGui(QtGui.QWidget):
         robot_config_file = file_path_prefix + 'robot_config.yaml'
         robot_yaml = yaml.ConfigParser(robot_config_file)
         self.robot_config = robot_yaml.get_by_key("robot")
-        print "in init", self.robot_config
         self.initUI()
 
     def initUI(self):
         hbox = QtGui.QHBoxLayout()
-        grid = QtGui.QGridLayout()
+        sqp_grid = QtGui.QGridLayout()
         grid1 = QtGui.QGridLayout()
 
+        # # Definition des Tracing Parameters widgets
+        # trust_box_size_label = QtGui.QLabel("Trust region size")
+        # max_trust_region_label = QtGui.QLabel("Max trust region size")
+        # trust_box_size = QtGui.QSpinBox(self)
+        # trust_box_size.setValue(self.sqp_config["trust_region_size"])
+        # max_trust_region = QtGui.QSpinBox(self)
+        # max_trust_region.setValue(self.sqp_config["max_trust_region_size"])
+        # window = QtGui.QGroupBox(self)
+        # window.setTitle("SQP solver Parameters")
+        # hbox.addLayout(sqp_grid)
+        # hbox.addLayout(grid1)
+
+
+        sqp_labels = []
+        sqp_spin_box = []
+        sqp_grid = QtGui.QGridLayout()
+        min_ = 0.00001
+        max_ = 1
         # Definition des Tracing Parameters widgets
-        trust_box_size_label = QtGui.QLabel("Trust region size")
-        max_trust_region_label = QtGui.QLabel("Max trust region size")
-        trust_box_size = QtGui.QSpinBox(self)
-        trust_box_size.setValue(self.sqp_config["trust_region_size"])
-        max_trust_region = QtGui.QSpinBox(self)
-        max_trust_region.setValue(self.sqp_config["max_trust_region_size"])
+        for key, value in self.sqp_config.iteritems():
+            sqp_labels.append({key :QtGui.QLabel(key)})
+            sqp_spin_box.append({key: QtGui.QDoubleSpinBox(self)})
+
+        sqp_spin_box = {k: v for d in sqp_spin_box for k, v in d.items()}
+        sqp_labels = {k: v for d in sqp_labels for k, v in d.items()}
+
+        sqp_param_count = 0
+        hbox.addLayout(sqp_grid)
+        for key, value in sqp_labels.iteritems():
+            sqp_grid.addWidget(sqp_labels[key], sqp_param_count, 0)
+            sqp_grid.addWidget(sqp_spin_box[key], sqp_param_count, 1)
+            if self.sqp_config[key] is str:
+                sqp_spin_box[key].setValue(float(self.sqp_config[key]))
+            sqp_spin_box[key].setRange(min_, max_)
+            self.connect(sqp_spin_box[key], QtCore.SIGNAL('valueChanged(double)'), self.change_value1)
+            sqp_param_count += 1
+
+
+        # trust_box_size_label = QtGui.QLabel("Trust region size")
+        # max_trust_region_label = QtGui.QLabel("Max trust region size")
+        # trust_box_size = QtGui.QSpinBox(self)
+        # trust_box_size.setValue(self.sqp_config["trust_region_size"])
+        # max_trust_region = QtGui.QSpinBox(self)
+        # max_trust_region.setValue(self.sqp_config["max_trust_region_size"])
+        # hbox.addLayout(sqp_grid)
+        # hbox.addLayout(grid1)
         window = QtGui.QGroupBox(self)
         window.setTitle("SQP solver Parameters")
-        hbox.addLayout(grid)
-        hbox.addLayout(grid1)
 
-        robot_group_label = QtGui.QLabel("Robot Planning Group")
-        self.robot_group_combo = QtGui.QComboBox(self)
-        temp = [x["name"] for x in self.robot_config["groups"]]
-        temp1 = [self.robot_config["group"][k]["joints"] for k in range(len(x)) for x in self.robot_config["groups"] if
-                 "sub-groups" in x]
-        print "temp", temp1
+        robot_labels = []
+        # robot_combo_box = []
+        robot_grid = QtGui.QGridLayout()
+        self.robot_combo_box =QtGui.QComboBox(self)
+        robot_label = QtGui.QLabel("Robot Planning Group")
 
-        # temp1 = [temp[y] for y in range(len(x))x for x in temp1]
-        flattened_list = [y for x in temp1 for y in x]
+        # Definition des Tracing Parameters widgets
+        for i in range(len(self.robot_config["groups"])):
+            for key, value in self.robot_config["groups"][i].iteritems():
+                if key == "name":
+                    # robot_labels.append({value: QtGui.QLabel(value)})
+                    self.robot_combo_box.addItem(value)
+                    # robot_grid.append({value: QtGui.QGridLayout()})
 
-        print "temp", flattened_list
-        self.robot_group_combo.addItems(temp)
-        self.robot_group_combo.addItems(flattened_list)
-        self.robot_group_combo.currentIndexChanged.connect(self.selectionchange)
-        grid.addWidget(trust_box_size_label, 0, 0)
-        grid.addWidget(trust_box_size, 0, 1)
-        grid.addWidget(max_trust_region_label, 1, 0)
-        grid.addWidget(max_trust_region, 1, 1)
+        hbox.addLayout(robot_grid)
+        robot_grid.addWidget(robot_label, 2, 0)
+        robot_grid.addWidget(self.robot_combo_box, 2, 1)
 
-        grid1.addWidget(robot_group_label, 0, 2)
-        grid1.addWidget(self.robot_group_combo, 0, 3)
+        self.robot_combo_box.currentIndexChanged.connect(self.selectionchange)
+
 
         window.setLayout(hbox)
 
-        # self.setLayout(hbox)
 
 
-        self.setGeometry(300, 300, 550, 300)
+        self.setGeometry(300, 300, 850, 300)
         self.setWindowTitle('Trajectory Planner')
 
         self.show()
 
+    def change_value1(self, val):
+       print val
     def selectionchange(self, i):
-        print "Items in the list are :"
 
-        # for count in range(self.cb.count()):
-        #     print self.cb.itemText(count)
-        print "Current index", i, "selection changed ", self.robot_group_combo.currentText()
-
+        print "Current index", i, "selection changed ", self.robot_combo_box.currentText()
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    ex = PlannerGui()
     sys.exit(app.exec_())
 
 

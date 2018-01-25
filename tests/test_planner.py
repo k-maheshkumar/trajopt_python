a86c902ca595be9c1e5e686a426ec3553ec3de3b
Mahesh Kumar Karikalan <maheshku@uni-bremen.de>
2018-01-25 19:03:13 +0100

    updated test planner with random joints and samples

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -6,9 +6,11 @@ from easydict import EasyDict as edict
 import yaml
 import numpy as np
 
-from scripts.Planner import Planner1 as planner
-
+from scripts.Planner import Planner as planner
+from collections import defaultdict
 import cvxpy
+import random
+import itertools
 
 import logging
 #
@@ -21,9 +23,29 @@ logger.addHandler(stream_handler)
 class Test_sqp_solver(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        with open("problem_1_joint.yaml", 'r') as config:
+        with open("problem.yaml", 'r') as config:
             cls.problem = edict(yaml.load(config))
         cls.planner = planner.TrajectoryOptimizationPlanner()
+
+        # np.random.seed(0)
+        length = 1
+        cls.samples = np.random.random_integers(20, 30, size=(1, length)).flatten()
+        cls.durations = np.random.random_integers(5, 20, size=(1, length)).flatten()
+        joints_random = np.random.random_integers(7, 20, size=(1, length)).flatten()
+        # print samples
+        # print durations
+        cls.joints = []
+
+        for i in range(len(joints_random)):
+
+            joints = defaultdict(lambda: defaultdict(dict))
+            for j in range(joints_random[i]):
+                joints[str(j)]["states"]["start"] = cls.random_float(-2, 2)
+                joints[str(j)]["states"]["end"] = cls.random_float(-2, 2)
+                joints[str(j)]["limit"]["lower"] = cls.random_float(-5, -2)
+                joints[str(j)]["limit"]["upper"] = cls.random_float(2, 5)
+                joints[str(j)]["limit"]["velocity"] = cls.random_float(10, 12)
+            cls.joints.append(edict(joints))
 
     def setUp(self):
         pass
@@ -31,79 +53,47 @@ class Test_sqp_solver(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def get_actual_result(self, joints, samples, duration, lower_joint, upper_joint, velocity, start, end):
-        lower_limit = lower_joint
-        upper_limit = upper_joint
-        problem = []
-        min_vel = -velocity
-        max_vel = velocity
-        min_vel *= duration / float(samples - 1)
-        max_vel *= duration / float(samples - 1)
-        print min_vel, max_vel
+    @classmethod
+    def random_float(self, low, high):
+        return random.random() * (high - low) + low
+
+    def get_actual_result(self, joints, samples, duration):
+        x = cvxpy.Variable((samples, len(joints)))
         cost = 0
         constraints = []
-        x = cvxpy.Variable((samples, joints))
-
-        for i in range(joints):
+        joints = joints.values()
+        for i in range(len(joints)):
             for t in range((samples - 1)):
+                min_vel = - joints[i].limit.velocity * duration / float(samples - 1)
+                max_vel = joints[i].limit.velocity * duration / float(samples - 1)
+                lower_limit = joints[i].limit.lower
+                upper_limit = joints[i].limit.upper
+                start = joints[i].states.start
+                end = joints[i].states.end
                 cost += cvxpy.sum_squares(x[t + 1, i] - x[t, i])
                 constraints += [x[t + 1, i] - x[t, i] <= max_vel,
                                 min_vel <= x[t + 1, i] - x[t, i]]
-                pro = cvxpy.Problem(cvxpy.Minimize(cost), constraints)
-                problem.append(pro)
-        constraints += [lower_limit <= x, x <= upper_limit]
-        constraints += [x[0, 0] == start, x[samples - 1, 0] == end]
+                constraints += [lower_limit <= x, x <= upper_limit]
+                constraints += [x[0, i] == start, x[samples - 1, i] == end]
 
         problem = cvxpy.Problem(cvxpy.Minimize(cost), constraints)
         problem.solve(solver=cvxpy.ECOS, verbose=False)
-        G = np.asarray(problem.get_problem_data(cvxpy.OSQP)[0]["F"].todense()).T
-        G = G[~np.all(G == 0, axis=1)]
-        ubG = np.asarray(problem.get_problem_data(cvxpy.OSQP)[0]["G"])
-
-        # print G.T
-        # print ubG
         return x.value.T
 
-    def test_solver_from_problem_file(self):
-        self.problem.samples = 10
-        self.problem.duration = 3
-        # self.actual_result =  np.array([-0.49197958, - 0.66418096,
-        #                                         - 0.83638088, - 1.00858013,
-        #                                         - 1.18077868, - 1.35297643,
-        #                                         - 1.52517484, - 1.69737578,
-        #                                         - 1.86957595, - 2.0417783])
-        self.actual_result = self.get_actual_result(len(self.problem.joints), self.problem.samples, self.problem.duration,
-                                                    self.problem.joints["lbr_iiwa_joint_1"].limit.lower,
-                                                    self.problem.joints["lbr_iiwa_joint_1"].limit.upper,
-                                                    self.problem.joints["lbr_iiwa_joint_1"].limit.velocity,
-                                                    self.problem.joints["lbr_iiwa_joint_1"].states.start,
-                                                    self.problem.joints["lbr_iiwa_joint_1"].states.end)
+    def test_random_joints_planning(self):
+        pass
+        # print joints["10"].states
+        for sample, duration, joints in itertools.izip(self.samples, self.durations, self.joints):
+            actual_result = self.get_actual_result(joints, sample, duration)
+            self.planner.init(joints=joints, samples=sample, duration=duration,
+                           solver=None, solver_config=None, solver_class=1,
+                           decimals_to_round=7, verbose=False)
+            self.planner.calculate_trajectory()
+            trajectory =  self.planner.get_trajectory().final.T
 
-        # print self.actual_result
-        self.planner.init(joints=self.problem.joints, samples=self.problem.samples, duration=self.problem.duration,
-                       solver=None, solver_config=None, solver_class=1,
-                       decimals_to_round=4, verbose=False)
-        # print self.planner.sqp_solver.G
-        # print self.planner.sqp_solver.lbG
-        # print self.planner.sqp_solver.ubG
-        # self.planner.display_problem()
-        self.planner.calculate_trajectory()
-        trajectory =  self.planner.get_trajectory().final.T
-        # print np.gradient(trajectory)
-        print "result ", trajectory
-        # print "initial ", self.planner.get_trajectory().initial.T
-
-        print np.ediff1d(trajectory)
-
-        print np.isclose(self.actual_result, trajectory, atol=0.01)
-        print np.linalg.norm(np.ediff1d(trajectory), np.inf)
+            self.assertEquals(np.isclose(actual_result, trajectory, atol=0.01).all(), True)
 
 
-        #
-    # def testSimpleMsg(self):
-    #     stream_handler.stream = sys.stdout
-    #     print("AA")
-    #     logging.getLogger().info("BB")
 
 if __name__ == '__main__':
     unittest.main()

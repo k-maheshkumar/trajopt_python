7b46c4daa9ecf979699413091c373ec70c613924
Mahesh Kumar Karikalan <maheshku@uni-bremen.de>
2018-03-09 17:20:39 +0100

    added nicer interface for the mongoDb driver

--- scripts/DB/Mongo_driver.py
+++ scripts/DB/Mongo_driver.py
@@ -1,42 +1,41 @@
 from pymongo import Connection
+from pymongo import MongoClient
 
 
 class MongoDriver():
-    def __init__(self):
-        # self.client = MongoClient('localhost', 27017)
-        self.client = Connection()
-        self.db_collection = self.client["trajectory_planner"]
-        self.db = self.db_collection.request
+    def __init__(self, db_name, host='localhost', port=27017):
+        self.client = MongoClient(host, port)
+        # self.client = Connection()
+        self.db_collection = self.client[db_name]
+        self.db = self.db_collection.db_name
 
-        request = {
-            "samples": 5,
-            "duration": 6,
-            "max_iteration": 1000,
-            "max_penalty": 500,
-            "max_delta": 5,
-            "joints": [
-                {"start": 0.2, "end": 0.7, "lower_joint_limit": -0.4, "upper_joint_limit": 1.1, "min_velocity": -0.1,
-                 "max_velocity": 0.1},
-                {"start": 0.4, "end": 0.9, "lower_joint_limit": -0.4, "upper_joint_limit": 1.1, "min_velocity": -0.1,
-                 "max_velocity": 0.1},
-            ]
-        }
-        # self.insert(request)
-        self.find()
-
-    def find(self):
-        peeps = self.db.find()
-        for p in peeps:
-            print p
-
+    def find(self, query):
+        return self.db.find_one(query)
 
     def insert(self, request):
+
         self.db.insert(request)
 
 
 def main():
 
-    db = MongoDriver()
+    db = MongoDriver("trajectory_planner")
+    request = {
+        "samples": 5,
+        "duration": 6,
+        "max_iteration": 1000,
+        "max_penalty": 500,
+        "max_delta": 5,
+        "joints": [
+            {"start": 0.2, "end": 0.7, "lower_joint_limit": -0.4, "upper_joint_limit": 1.1, "min_velocity": -0.1,
+             "max_velocity": 0.1},
+            {"start": 0.4, "end": 0.9, "lower_joint_limit": -0.4, "upper_joint_limit": 1.1, "min_velocity": -0.1,
+             "max_velocity": 0.1},
+        ]
+    }
+    # db.insert(request)
+    print(db.find({"samples": 5}))
+
 
 if __name__ == '__main__':
     main()

0e40e249fde1c8b1bc0563ed4dbbe731cafdb69f
Mahesh Kumar Karikalan <maheshku@uni-bremen.de>
2018-05-18 12:01:07 +0200

    refracted don bot example to use values from config file

--- scripts/examples/planner_don_bot.py
+++ scripts/examples/planner_don_bot.py
@@ -45,13 +45,12 @@ class PlannerExample:
         #                                                  position=[0, 0, 0.0])
         # #
         # self.box_id = self.planner.add_constraint("box1", shape=self.planner.world.BOX, size=[0.05, 0.05, 0.1],
-        #                                           position=[0, 0.3, 0.62], mass=100)
-        # self.box_id1 = self.planner.add_constraint("box2", shape=self.planner.world.BOX, size=[0.05, 0.05, 0.2],
-        #                                           position=[0.15, -0.2, 0.62], mass=100)
+        #                                           position=[-0.15, 0.3, 0.62], mass=100)
+        # self.box_id1 = self.planner.add_constraint("box2", shape=self.planner.world.BOX, size=[0.05, 0.05, 0.1],
+        #                                           position=[0.0, -0.2, 0.62], mass=100)
         # self.box_id2 = self.planner.add_constraint("box3", shape=self.planner.world.BOX, size=[0.1, 0.05, 0.1],
-        #                                           position=[0.15, 0.4, 1], mass=100)
-        # self.box_id2 = self.planner.add_constraint("box3", shape=self.planner.world.BOX, size=[0.1, 1, 0.02],
-        #                                           position=[0.15, 0.4, 0.9], mass=100)
+        #                                           position=[0.0, 0.4, 1], mass=100)
+
         shelf_item_prefix = home + "/catkin_ws/src/shelf_item_descriptions/urdf/"
         salt_urdf = shelf_item_prefix + "salt.urdf"
         gel_urdf = shelf_item_prefix + "duschGel.urdf"
@@ -90,56 +89,25 @@ class PlannerExample:
         # self.planner.world.step_simulation_for(0.2)
 
     def run(self):
-        from collections import OrderedDict
+        start_state = "below_shelf1"
+        goal_state = "above_shelf1"
 
-        start_state = OrderedDict()
-        goal_state = OrderedDict()
+        start_state = "below_shelf"
+        goal_state = "above_shelf"
+        group = "full_body"
 
-        start_state["odom_x_joint"] = 0.1
-        start_state["odom_y_joint"] = 0.3
-        start_state["odom_z_joint"] = 0.01
+        self.planner.reset_robot_to(start_state, group)
 
-        start_state["ur5_shoulder_pan_joint"] = 1.9823357809267463
-        start_state["ur5_shoulder_lift_joint"] = -2.4299975516996142
-        start_state["ur5_elbow_joint"] = -1.9762726255540713
-        start_state["ur5_wrist_1_joint"] = 0.8666279970481103
-        start_state["ur5_wrist_2_joint"] = 1.5855963769735366
-        start_state["ur5_wrist_3_joint"] = -1.5770985888989753
-        start_state["gripper_joint"] = 0
-
-        start_state["gripper_base_gripper_left_joint"] = 0
-        start_state["ur5_ee_fixed_joint"] = 1.5704531145724918
-
-        goal_state["odom_x_joint"] = 0.1
-        goal_state["odom_y_joint"] = 0.3
-        goal_state["odom_z_joint"] = 0.01
-
-        goal_state["ur5_shoulder_pan_joint"] = 1.9823357809267463
-        goal_state["ur5_shoulder_lift_joint"] = -1.8299975516996142
-        goal_state["ur5_elbow_joint"] = -1.9762726255540713
-        goal_state["ur5_wrist_1_joint"] = 0.8666279970481103
-        goal_state["ur5_wrist_2_joint"] = 1.5855963769735366
-        goal_state["ur5_wrist_3_joint"] = -1.5770985888989753
-        #
-        goal_state["gripper_joint"] = 0
-        goal_state["gripper_base_gripper_left_joint"] = 0
-
-        group = goal_state.keys()
+        goal_state = "above_shelf1"
+        group = "ur5_arm"
 
         duration = 10
         samples = 20
         collision_check_distance = 0.15
         collision_safe_distance = 0.1
 
-        self.planner.world.reset_joint_states(self.robot_id, start_state.values(), start_state.keys())
-
-        group1 = goal_state.keys()[3:]
-        start_state1 = start_state.values()[3:]
-        goal_state1 = goal_state.values()[3:]
-
-        _, status, trajectory = self.planner.get_trajectory(samples=samples, duration=duration,
-                                                            # group=group, goal_state=goal_state,
-                                                            group=group1, goal_state=goal_state1,
+        _, status, trajectory =  self.planner.get_trajectory(samples=samples, duration=duration,
+                                                            group=group, goal_state=goal_state,
                                                             collision_safe_distance=collision_safe_distance,
                                                             collision_check_distance=collision_check_distance)
         print("is trajectory free from collision: ", status)
